1d6262a95aebcd5bf5fa4a254e1897abda990b52
Zhad-Yoghod <zhadyoghod@gmail.com>
2025-08-17 01:30:54 -0600

    Pantalla de Inicio
    Pantalla de Inicio creada.
    Contiene:
    - Iniciar juego (Con el minijuego de Pipes por defecto).
    - Salir del juego.
    - Diseño... ¿guay?

---
+++ Shared/inicio.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_jugar_pressed() -> void:
+	get_tree().change_scene_to_file("res://Games/Pipes/pipes-main.tscn") # Pasar a la siguiente escena.
+
+
+func _on_salir_pressed() -> void:
+	get_tree().quit() # Salir del juego.
