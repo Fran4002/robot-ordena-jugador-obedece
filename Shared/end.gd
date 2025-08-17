8987ae2b1d58bbad60a267f0feaa49d64bc65728
Zhad-Yoghod <zhadyoghod@gmail.com>
2025-08-17 11:58:44 -0600

    Acomodo de los niveles.
    Se ajustó los niveles con relación al sprite de la TV.
    * Nivel de los bichos: ya comprendí porque no se acababa el nivel. Checar documentación.

--- Shared/end.gd
+++ Shared/end.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_reiniciar_pressed() -> void:
-	get_tree().change_scene_to_file("res://Games/Pipes/pipes-main.tscn") # Volver al primer escenario
+	get_tree().change_scene_to_file("res://Games/Cables/cables-main.tscn") # Volver al primer escenario
 
 
 func _on_salir_pressed() -> void:
